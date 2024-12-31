cb1e34312eb3596402689ed711f1fa801e331e65
WangariNelly <nellywangari30@gmail.com>
2024-12-31 13:22:38 +0300

    Form login changes

--- src/main/java/com/security/securityTutorial/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/securityTutorial/security/ApplicationSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.security.securityTutorial.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static javax.management.Query.and;
@@ -12,14 +18,38 @@ import static javax.management.Query.and;
 @Configuration
 //@EnableWebSecurity
 public class ApplicationSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
           http
-                .authorizeHttpRequests()
+                .authorizeRequests()
                  .requestMatchers("/", "index")
                  .permitAll()
                  .anyRequest()
-                 .authenticated();
+                 .authenticated()
+        .and()
+                  .formLogin();
+
         return http.build();
     }
+     @Bean
+    protected UserDetailsService userDetailsService() {
+         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build());
+         manager.createUser(User.withUsername("admin")
+                 .password(passwordEncoder.encode("admin123"))
+                 .roles("ADMIN")
+                 .build());
+         return manager;
+     };
 }
